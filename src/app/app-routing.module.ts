04072cf0660b91ba23ca619a7b62ff4c2eb7a18e
djayaram1995 <jayaramdeenu@gmail.com>
2023-10-14 15:24:10 +0530

    Changes for host

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'counter',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'innerApp',
+        exposedModule: './CounterModule',
+      }).then((m) => m.CounterModule).catch(err => console.log(err));
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
